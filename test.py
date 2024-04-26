6e4442698d077edfeb918f724cf5475f9c3a1065
Daniel Pang <dp3228@nyu.edu>
2024-04-26 17:40:47 -0400

    finished drag and dropping

--- test.py
+++ test.py
@@ -15,7 +15,8 @@ zobrist = zobrist()
 transpositions = {}
 ct=0
 #read from database
-pgn = open('games/caissabase.pgn')
+pgn = open('game.pgn')
+#pgn = open('games/caissabase.pgn')
 mastergame = ""
 games = []
 gc=0
@@ -24,12 +25,32 @@ while((game != None) & (gc < 500)):
     gc+=1
     games.append(mastergame)
 
-from flask import Response, Flask
+from flask import Response, request, Flask, render_template, make_response, jsonify
 
 def to_svg(s):
   return base64.b64encode(chess.svg.board(board=s.board).encode('utf-8')).decode('utf-8')
 
 app = Flask(__name__)
+@app.route("/interact")
+def serve():
+    return render_template('home.html')
+
+@app.route("/human", methods = ['POST'])
+def play():
+    if request.method == 'POST':
+        """modify/update the information for <user_id>"""
+        # you can use <user_id>, which is a str but could
+        # changed to be int or whatever you want, along
+        # with your lxml knowledge to make the required
+        # changes
+        print(request.json) # a multidict containing POST data
+    newb = ['bb','bp','bk']
+    res = jsonify(board=newb)
+    return res
+    return render_template("home.html")
+
+def make_move(move):
+    pass
 @app.route("/")
 def home():
     global board
@@ -46,9 +67,7 @@ def home():
     return ret
 
 
-@app.route("/human")
-def play():
-    pass
+
 def computermove(s):
     global game
     if board.fullmove_number < 5:
@@ -106,19 +125,18 @@ def playMove(state):
     for i in range(4):
         scores = []
         if(bestmove != None):
-            print("starting with bestmove:",bestmove)
+            print("starting with bestmove:",bestmove, "at depth ", i)
             if(time.time()-start > 5):
                 print("TIMES UP")
                 #return bestmove
             b.push(bestmove)
-            scores.append((-negamax(state,i,b.turn,-math.inf,math.inf),bestmove))
+            scores.append((-negamax(state,i,not b.turn,-math.inf,math.inf),bestmove))
             b.pop()
 
         for m in state.orderMoves():
             b.push(m[0])
-            scores.append((-negamax(state,i,b.turn,-math.inf,math.inf),m[0]))
+            scores.append((-negamax(state,i,not b.turn,-math.inf,math.inf),m[0]))
             b.pop()
-
         sort = sorted(scores, key = lambda x:x[0], reverse = True)
         bestmove = sort[0][1]
         topeval = sort[0][0]
@@ -139,7 +157,7 @@ def negamax(s, depth, turn,alpha, beta):
     ct +=1
     b = s.board
     if(depth ==0 or b.is_game_over()):
-        return s.evaluate() if turn else -s.evaluate()
+        return s.evaluate()
     ordered = s.orderMoves()
     maxval = -math.inf
     for move in ordered:
@@ -149,27 +167,21 @@ def negamax(s, depth, turn,alpha, beta):
         hash = zobrist.gen_zobhash(s.board)
         if(hash in transpositions):
             s.board.pop()
-            print("using transposition",hash)
             if(depth == 4):
                 print("move:", move[0], "givenvalue", transpositions[hash], "capturevalue",move[1],"for player","white" if s.board.turn else "black")
             maxval = transpositions[hash]
         else:
-            x = -negamax(s,depth - 1, not turn, -beta,-alpha)
-            if(x>maxval):
-                #print("move:", move[0], "givenvalue", x, "capturevalue",move[1],"for player","white"if s.board.turn else "black")
-                maxmove = move[0]
-                maxval = x
+            maxval = max(-negamax(s,depth - 1, not turn, -beta,-alpha),maxval)
             s.board.pop()
-            transpositions[hash] = maxval
+            transpositions[hash] = maxval if b.turn else -maxval
         #maxval = max(-negamax(s,depth - 1, -beta,-alpha),maxval)
         #print("current maxval", maxval)
         
         alpha = max(alpha, maxval)
-        if(alpha > beta):
+        if(alpha < beta):
                 #print("prune tree with move ", move[0],"alpha", alpha, "beta", beta)
                 """prune"""
                 break    
-    #print("returning from depth", depth, "with bestmove ", maxmove, "for player ","white"if s.board.turn else "black")
     return maxval
 
 def playOpening(s):
@@ -180,15 +192,16 @@ def playOpening(s):
     print(s.board())
     foundgame = 0
     for game in games:
-        while(game.next()!=None):
-            if(game.board().fen() == fen):
-                foundgame = game.next().move
-            if(time.time()-start >=.5):
-                if(foundgame):
-                    return foundgame
-                else:
-                    return -1
-            game = game.next()
+        if(game):
+            while(game.next()!=None):
+                if(game.board().fen() == fen):
+                    foundgame = game.next().move
+                if(time.time()-start >=.5):
+                    if(foundgame):
+                        return foundgame
+                    else:
+                        return -1
+                game = game.next()
     return -1
 
 
